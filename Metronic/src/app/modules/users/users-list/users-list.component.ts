ef8394a9e82628b891825715e212c4fb1f60fec0
jborelli95 <jborelli95@gmail.com>
2026-03-13 18:08:26 -0300

    Working on adding users and user lists

--- Metronic/src/app/modules/users/users-list/users-list.component.ts
+++ Metronic/src/app/modules/users/users-list/users-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { UsersAddComponent } from '../users-add/users-add.component';
 
 @Component({
   selector: 'app-users-list',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./users-list.component.scss']
 })
 export class UsersListComponent {
+  constructor(
+    private modalService:NgbModal
+  ){}
 
+  registerUser(){
+    const modalRef = this.modalService.open(UsersAddComponent, {centered: true, size: 'md'});
+  }
 }
