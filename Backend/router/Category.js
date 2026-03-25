cb80519de952fe251a61986f142d0eed32ca5145
jborelli95 <jborelli95@gmail.com>
2026-03-25 18:06:41 -0300

    Frontend: List and edit of category

--- Backend/router/Category.js
+++ Backend/router/Category.js
@@ -4,13 +4,13 @@ import auth from '../services/auth.js';
 import multiparty from 'connect-multiparty';
 import CategoryController from '../controllers/CategoryController.js';
 
-var path = multiparty({uploadDir : './uploads/category'});
+var path = multiparty({uploadDir : './uploads/categories'});
 
 const router = express.Router();
 
 /**categories/register */
 router.post("/register", [auth.verifyAdmin, path], CategoryController.register);
-router.post("/update", [auth.verifyAdmin, path], CategoryController.register);
+router.post("/update", [auth.verifyAdmin, path], CategoryController.update);
 router.get("/list", [auth.verifyAdmin], CategoryController.list);
 router.delete("/remove/:id", [auth.verifyAdmin], CategoryController.remove);
 
