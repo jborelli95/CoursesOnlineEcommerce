0b6a7dfa286a1eca83ccd93229c561f798e922a5
jborelli95 <jborelli95@gmail.com>
2026-03-30 09:28:17 -0300

    Finished crud category

--- Metronic/src/app/modules/category/category-delete/category-delete.component.ts
+++ Metronic/src/app/modules/category/category-delete/category-delete.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CategoryService } from '../service/category.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-category-delete',
   templateUrl: './category-delete.component.html',
   styleUrls: ['./category-delete.component.scss']
 })
-export class CategoryDeleteComponent {
+export class CategoryDeleteComponent implements OnInit{
+  @Input() dCategory: any;
+  @Output() categoryD: EventEmitter<any> = new EventEmitter();
 
+  constructor(
+    private toastr: ToastrService,
+    private categoryService: CategoryService,
+    private modal: NgbActiveModal,
+  ) { }
+  ngOnInit(): void {
+    console.log(this.dCategory);
+  }
+
+  deleteUser() {
+    this.categoryService.removeCategory(this.dCategory._id).subscribe((resp: any) => {
+      console.log(resp);
+      this.categoryD.emit('');
+      this.modal.close();
+      this.toastr.success("The user was successfully deleted", "Success:");
+    })
+  }
+
+  modalClose() {
+    this.modal.close();
+  }
 }
--- Metronic/src/app/modules/category/category-edit/category-edit.component.ts
+++ Metronic/src/app/modules/category/category-edit/category-edit.component.ts
@@ -10,79 +10,80 @@ import { CategoryService } from '../service/category.service';
   styleUrls: ['./category-edit.component.scss']
 })
 export class CategoryEditComponent {
-   @Input() editCategory: any;
-    @Output() categoryE: EventEmitter<any> = new EventEmitter();
-  
-    file_image: any;
-    preview_image: any = "assets/media/avatars/300-6.jpg";
-    editForm: FormGroup;
-  
-    constructor(
-      private fb: FormBuilder,
-      private toastr: ToastrService,
-      private modal: NgbActiveModal,
-      private categoryService: CategoryService,
-    ) {}
-  
-    ngOnInit(): void {
-      console.log(this.editCategory);
-  
-       this.editForm = this.fb.group({
-        title: [this.editCategory.title, [Validators.required, Validators.minLength(2)]],
-        state: [this.editCategory.state, [Validators.required]],
-      });
-  
-      if(this.editCategory.image){
-        this.preview_image = this.editCategory.image;
-      }
-  
+  @Input() editCategory: any;
+  @Output() categoryE: EventEmitter<any> = new EventEmitter();
+
+  file_image: any;
+  preview_image: any = "assets/media/avatars/300-6.jpg";
+  editForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private toastr: ToastrService,
+    private modal: NgbActiveModal,
+    private categoryService: CategoryService,
+  ) { }
+
+  ngOnInit(): void {
+    console.log(this.editCategory);
+    console.log(this.editCategory.state);
+
+    this.editForm = this.fb.group({
+      title: [this.editCategory.title, [Validators.required, Validators.minLength(2)]],
+      state: [this.editCategory.state.toString(), [Validators.required]],
+    });
+
+    if (this.editCategory.image) {
+      this.preview_image = this.editCategory.image;
     }
-  
-    submit(){
-      if(this.editForm.invalid){
-        this.editForm.markAllAsTouched();
-        this.toastr.error("Complete all required fields", "Error!: ");
-        return;
-      }
-  
-      const formData = new FormData();
-  
-      formData.append('_id', this.editCategory._id);
-      formData.append('title', this.editForm.get('title')?.value);
-      formData.append('state', this.editForm.get('state')?.value);
-      if(this.file_image){
-        formData.append('image', this.file_image);
-      }  
-  
-      console.log(formData);
-  
-      this.categoryService.updateCategory(formData).subscribe({
-        next: (value:any) => {
-          console.log(value);
-          this.categoryE.emit(value.user);
-          this.modal.close();
-          this.toastr.success("The category was successfully updated", "Success:");
-        }
-      });
+
+  }
+
+  submit() {
+    if (this.editForm.invalid) {
+      this.editForm.markAllAsTouched();
+      this.toastr.error("Complete all required fields", "Error!: ");
+      return;
     }
-  
-    processImage($event: any) {
-      if ($event.target.files[0].type.indexOf("image") < 0) {
-        console.log($event.target.files[0]);
-        this.toastr.error("Only files '.jpg, .png and .jpeg' accepted", "Error validation")
-        return;
+
+    const formData = new FormData();
+
+    formData.append('_id', this.editCategory._id);
+    formData.append('title', this.editForm.get('title')?.value);
+    formData.append('state', this.editForm.get('state')?.value);
+    if (this.file_image) {
+      formData.append('image', this.file_image);
+    }
+
+    console.log(formData);
+
+    this.categoryService.updateCategory(formData).subscribe({
+      next: (value: any) => {
+        console.log(value);
+        this.categoryE.emit(value.category);
+        this.modal.close();
+        this.toastr.success("The category was successfully updated", "Success:");
       }
+    });
+  }
+
+  processImage($event: any) {
+    if ($event.target.files[0].type.indexOf("image") < 0) {
       console.log($event.target.files[0]);
-      this.file_image = $event.target.files[0];
-  
-      let reader = new FileReader();
-      reader.readAsDataURL(this.file_image);
-      reader.onloadend = () => {
-        this.preview_image = reader.result;
-      }
+      this.toastr.error("Only files '.jpg, .png and .jpeg' accepted", "Error validation")
+      return;
     }
-  
-    modalClose(){
-      this.modal.close();
+    console.log($event.target.files[0]);
+    this.file_image = $event.target.files[0];
+
+    let reader = new FileReader();
+    reader.readAsDataURL(this.file_image);
+    reader.onloadend = () => {
+      this.preview_image = reader.result;
     }
+  }
+
+  modalClose() {
+    this.modal.close();
+  }
 }
